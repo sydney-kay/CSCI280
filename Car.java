66fa1e7ac666b2597b759b5a3db362ca7725133c
Derek-Shannon <shannonderek56@gmail.com>
2024-02-28 15:45:21 -0600

    Outline of Car

--- Car.java
+++ Car.java
@@ -1,3 +1,13 @@
-public class Car {
-    
+public class Car extends Vehicle{
+    private int numDoors;
+
+    public Car(String make, String model, int year, int numDoors){
+        Vehicle.super(make, model, year);
+        this.numDoors = numDoors;
+    }
+
+    @Override
+    printInfo(){
+        
+    }
 }
