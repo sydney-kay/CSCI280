3024316442f0dd25a644da99cda6d4d773ca0be4
Derek-Shannon <shannonderek56@gmail.com>
2024-02-28 15:46:11 -0600

    Temp Fix Car

--- Car.java
+++ Car.java
@@ -2,12 +2,12 @@ public class Car extends Vehicle{
     private int numDoors;
 
     public Car(String make, String model, int year, int numDoors){
-        Vehicle.super(make, model, year);
+        super(make, model, year);
         this.numDoors = numDoors;
     }
 
     @Override
-    printInfo(){
+    public void printInfo(){
         
     }
 }
