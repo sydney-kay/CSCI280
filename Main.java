c585758ab5e76e70d28cc6937029310724fe6518
Derek-Shannon <shannonderek56@gmail.com>
2024-02-28 15:51:06 -0600

    Added base main

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main(){
+public class Main{
     public static void main(String []args){
-        
+        Vehicle vehicle = new Vehicle("Ford", "F150", 2009);
+        Car car = new Car("Honda", "Civic", 2021, 4);
+        Motorcycle motorcycle = new Motorcycle();
     }
 }
