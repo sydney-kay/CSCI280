f909940f9b64e2a2e34a0c6a45b0d102c8035cac
Sydney <fadingstardg@gmail.com>
2024-02-28 15:45:06 -0600

    Vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,15 @@
 public class Vehicle {
-    
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void printInfo(){
+        System.out.println("");
+    }
 }
