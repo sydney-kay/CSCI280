2e4c2e785a742daf2457da15de83c8e9ff5047b2
Sydney <fadingstardg@gmail.com>
2024-02-28 15:52:00 -0600

    Created Motorcycle

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,3 +1,13 @@
-public class Motorcycle {
-    
+public class Motorcycle extends Vehicle{
+    private Boolean isCruiser;
+
+    public Motorcycle(String make, String model, int year, Boolean isCruiser){
+        super(make, model, year);
+        this.isCruiser = isCruiser;
+    }
+
+    @Override
+    public void printInfo(){
+        
+    }
 }
