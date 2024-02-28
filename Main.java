177d9fc8e75c0a7f69705cc1a5f6bea58d56238a
Derek-Shannon <shannonderek56@gmail.com>
2024-02-28 15:56:00 -0600

    Added Prints

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+// CS-280 Sp24
+// Derek Shannon & Sydney Nilles
+// Assignment #6
+// 03/01/2024
+
 public class Main{
     public static void main(String []args){
         Vehicle vehicle = new Vehicle("Ford", "F150", 2009);
         Car car = new Car("Honda", "Civic", 2021, 4);
-        Motorcycle motorcycle = new Motorcycle();
+        Motorcycle motorcycle = new Motorcycle("Harley", "Road King", 2024, true);
+        vehicle.printInfo();
+        car.printInfo();
+        motorcycle.printInfo();
     }
 }
