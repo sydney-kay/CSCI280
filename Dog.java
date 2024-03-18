0a776102622b08051d104d6c6bb0121a25006edc
Derek-Shannon <shannonderek56@gmail.com>
2024-03-18 10:35:55 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,3 +1,12 @@
 public class Dog extends Animal{
-    
+    public Dog(String name){
+        Animal.super(name);
+    }
+    @Override
+    public void eat(){
+        System.out.println("Nom Nom");
+    }
+    public void sleep(){
+        System.out.println("zzzzzzzzzzz");
+    }
 }
