718dd9ce05574d1e45eb0370e555e7796972a633
Sydney <fadingstardg@gmail.com>
2024-04-11 16:02:01 -0500

    Assignment 9

--- Assignment 9/Main.java
+++ Assignment 9/Main.java
@@ -7,14 +7,22 @@ public class Main{
     public static void main(String[]args){
         Scanner nelson = new Scanner(System.in);
         int num1 = 0;
-        for(int i = 0; i < 2; i++){
+        int counter = 0;
+        int numInvalid = 0;
+        while(counter < 2){
             System.out.println("Please enter a number: ");
             try {
                 num1 += nelson.nextInt();
+                counter ++;
             } catch (Exception e) {
-                System.out.println("Incorrect input, please enter a number.");
+                System.out.print("Incorrect input. ");
+                nelson.nextLine();
+                numInvalid ++;
             }     
         }
+        System.out.println("The sum is: " + num1);
+        System.out.println("Number of invalid inputs: " + numInvalid);
+        nelson.close();
         
     }
 }
