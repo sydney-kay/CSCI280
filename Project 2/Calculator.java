2205366df708680942d1bba349e43928583ff9c9
Sydney <fadingstardg@gmail.com>
2024-04-16 11:46:19 -0500

    Outlined Methods

--- Project 2/Calculator.java
+++ Project 2/Calculator.java
@@ -8,6 +8,44 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class Calculator {
-    private ArrayList<String> output = new ArrayList<String>();
+    private ArrayList<String> display;
     private double result;
+
+    public Calculator(){
+        display = new ArrayList<String>();
+        result = 0.0;
+    }
+
+    public void addition(){
+        // Add all numbers next to a '+' sign
+    }
+
+    public void subtraction(){
+        //substract all numbers next to a '-' sign
+    }
+
+    public void multiply(){
+        // multiply all numbers next to a '*'
+    }
+
+    public void divide(){
+        // divide all numbers next to a '/'
+    }
+
+    public void negative(){
+        // set number to a negative value
+    }
+
+    public void delete(){
+        display.remove(display.size());
+    }
+
+    public void clearCalculator(){
+        display.clear();
+    }
+
+    public void equal(){
+        //compute results & display on screen
+        // basically call all the other methods depending on what's in display
+    }
 }
