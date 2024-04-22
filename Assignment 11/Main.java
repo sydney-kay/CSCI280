6d800509b3ec1476d37fd52ad3f2a4bcd88313e4
Sydney <fadingstardg@gmail.com>
2024-04-21 22:42:41 -0500

    Updated Assignment 11

--- Assignment 11/Main.java
+++ Assignment 11/Main.java
@@ -1,12 +1,24 @@
+import java.io.*;
 import java.util.Scanner;
-import java.io.OutputStreamWriter;
-
 
 public class Main {
     public static void main(String[]args){
-        
+        try {
+            Scanner nelson = new Scanner(System.in);
+            FileOutputStream outputFile = new FileOutputStream("output.txt");
+            OutputStreamWriter outputWriter = new OutputStreamWriter(outputFile);
 
-        // prompt the user
-        System.out.println("Please write what you would like to be saved as a file.");
-    }    
+            // prompt user
+            System.out.println("Please enter the text you would like in the file, press enter when you are done. ");
+            String input = nelson.nextLine();
+            outputWriter.write(input.substring(0,51));
+
+            // close all the things that need to be closed
+            nelson.close();
+            outputWriter.close();
+        } catch (IOException e) {
+            // TODO: handle exception
+        }
+        
+    }
 }
