5e6236b0c0e856a4c0d64839da0e690f1989c308
Sydney <fadingstardg@gmail.com>
2024-04-21 21:44:22 -0500

    Created Assignment 11

---
+++ Assignment 11/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main(String[]args){
+        
+
+        // prompt the user
+        System.out.println("Please write what you would like to be saved as a file.");
+    }    
+}
