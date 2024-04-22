f0ffbf776165b72981a8428ab5f42e4ce6ee9fe8
Sydney <fadingstardg@gmail.com>
2024-04-21 23:38:43 -0500

    Updated Assignment 11

--- Assignment 11/Main.java
+++ Assignment 11/Main.java
@@ -1,3 +1,8 @@
+
+// CS-280 Sp24
+// Sydney Nilles
+// Assignment 10 - due 04/22/2024
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -11,13 +16,19 @@ public class Main {
             // prompt user
             System.out.println("Please enter the text you would like in the file, press enter when you are done. ");
             String input = nelson.nextLine();
-            outputWriter.write(input.substring(0,51));
+
+            // by rough estimation and online confirmation, strings are 2 bytes each, so a string of 25 length would be roughly 50 bytes
+            if (input.length() > 25){
+                outputWriter.write(input.substring(0,26));
+            } else {
+                outputWriter.write(input);
+            }
 
             // close all the things that need to be closed
             nelson.close();
             outputWriter.close();
         } catch (IOException e) {
-            // TODO: handle exception
+            System.out.println("IOException Error.");
         }
         
     }
