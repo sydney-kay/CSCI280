52109633e3533d9922e21367f30f92dcc974d431
Derek-Shannon <shannonderek56@gmail.com>
2024-04-23 12:15:24 -0500

    Removed outputs
    adjusted spacing also

--- Project 2/Calculator.java
+++ Project 2/Calculator.java
@@ -187,16 +187,15 @@ public class Calculator {
         }
         else if(type=="decimal"){
             for(int i = displaylist.size()-1; i>=0; i--){ //reverse loops through displaylist
-                System.out.print(displaylist.get(i));
                 switch(findType(displaylist.get(i))){
                     case "number": 
                         break;
                     case "operator": 
-                        if(displaylist.size()-1 == i){
+                        if(displaylist.size()-1 == i){ //adds 0 if there is no number before decimal
                             displaylist.add("0");
                         }
                         displaylist.add(e.getActionCommand());
-                        i=-1;
+                        i = -1;
                         break;
                     case "decimal":
                         i=-1;
@@ -207,11 +206,10 @@ public class Calculator {
                         System.out.println("Error!");
                         break;
                 }
-                if(i==0){
+                if(i == 0){
                     displaylist.add(e.getActionCommand());
                 }
             }
-            System.out.println();
         }
         else if(type=="+/-"){
             //donesn't add +/- if there isn't a number
@@ -243,7 +241,6 @@ public class Calculator {
                 }
             }
         }
-        System.out.println(displaylist);
         updateDisplay();
     }
 
